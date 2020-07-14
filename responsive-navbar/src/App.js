eaecaa00ef7ba2a093187c8e88a3ae9f0d1e68e2
jaymar85 <jermark851@gmail.com>
2020-07-13 23:52:35 -0500

    updated navbar

--- responsive-navbar/src/App.js
+++ responsive-navbar/src/App.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Toolbar from './components/Toolbar/Toolbar';
+import SideDrawer from './components/SideDrawer/SideDrawer';
+import Backdrop from './components/Backdrop/Backdrop';
 
-function App() {
-  return (
-    <div className="App">
-      <Toolbar />
-      <main>
-        <p>This is the page content!</p>
-      </main>
-    </div>
-  );
+class App extends Component {
+  state = {
+    sideDrawerOpen: false
+  };
+
+  drawerToggleClickHandler = () => {
+    this.setState((prevState) => {
+      return {sideDrawerOpen: !prevState.sideDrawerOpen}
+    });
+  };
+
+  render() {
+    let sideDrawer;
+    let backdrop;
+
+    if (this.state.sideDrawerOpen) {
+      sideDrawer = <SideDrawer />;
+      backdrop = <Backdrop />
+    }
+    return (
+      <div style={{height: '100%'}}>
+        <Toolbar drawerClickHandler={this.drawerToggleClickHandler} />
+        {sideDrawer}
+        {backdrop}
+        <main>
+          <p>This is the page content!</p>
+        </main>
+      </div>
+    );
+  };
 }
 
 export default App;
