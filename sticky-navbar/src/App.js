168e1c9b7757e73ce2d921b7903e6b762c7a732d
jaymar85 <jermark851@gmail.com>
2020-08-11 21:04:40 -0500

    update project

---
+++ sticky-navbar/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/navbar/Navbar'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar />
+      Hello World
+    </div>
+  );
+}
+
+export default App;
---
+++ sticky-navbar/src/components/navbar/Navbar.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './navbar.css';
+
+class Navbar extends Component {
+    constructor() {
+        super();
+        this.state = {
+            addName: 'sticky'
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.listenScrollEvent)
+    }
+
+    listenScrollEvent = e => {
+        if (window.scrollY > 0) {
+          this.setState({addName: 'sticky'})
+        } else {
+          this.setState({addName: ''})
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+            <header onScroll={this.listenScrollEvent} className={this.state.addName}>
+                <a href="#home" className="logo">Logo</a>
+                <ul>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#service">Services</a></li>
+                    <li><a href="#portfolio">Portfolio</a></li>
+                    <li><a href="#team">Team</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </header>
+            <section className="banner"></section>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
+
